bcde50fdb4b229fbdef004823a38bb072c091b27
Joseph <tsui.ming.hei@ospicon.com>
2018-06-19 11:14:14 +0800

    commit

---
+++ www/sts300.js
@@ -0,0 +1,18 @@
+var DeviceSensorLoader = function(require, exports, module) {
+  var exec = require('cordova/exec');
+
+  var intervalId;
+  
+  function DeviceSensor() {}
+
+  DeviceSensor.prototype.beep = function(success, failure) {
+    exec(success, failure, 'AndroidSensorManager', 'stop', []);
+  };
+  
+  var deviceSensor = new DeviceSensor();
+  module.exports = deviceSensor;
+};
+
+DeviceSensorLoader(require, exports, module);
+
+cordova.define("cordova/plugin/Sts300", DeviceSensorLoader);
